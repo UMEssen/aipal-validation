0a86826bfa3876fb80a22413eeb4f15869c1601c
Merlin Engelke <merlin.engelke@uk-essen.de>
2025-03-28 15:02:29 +0100

    tmp bump

--- tmp.cmd
+++ tmp.cmd
@@ -4,3 +4,9 @@ python -m aipal_validation --step sampling --root_dir /data
 python -m aipal_validation --step test --root_dir /data
 
 python -m aipal_validation --step test --root_dir /data --eval_all
+
+# train outlier
+python -m aipal_validation --step train --root_dir /data --config aipal_validation/config/config_outlier.yaml
+
+# test outlier
+python -m aipal_validation --sample aipal_validation/config/sample.json --model_dir aipal_validation/outlier --config aipal_validation/config/config_outlier.yaml
